b29237628306ac79deaf6c146152bc8a90acdd9c
v1pung <kaby.spb1@gmail.com>
2024-07-24 21:09:01 +0300

    Added strings module & multiplier func in nums

--- first/nums.py
+++ first/nums.py
@@ -6,6 +6,11 @@ def minus(a, b):
     return a - b
 
 
+def multi(a, b):
+    return a * b
+
+
 if __name__ == '__main__':
     print(plus(2, 2))
     print(minus(2, 2))
+    print(multi(2, 3))
---
+++ first/strings.py
@@ -0,0 +1,3 @@
+def up(text):
+    return text.upper()
+
